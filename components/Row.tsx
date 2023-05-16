971fcdceea233f0f90062723a81d772dd760ad6d
Marvin <mk306@hdm-stuttgart.de>
2023-05-16 16:03:00 +0200

    exercise 1

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,8 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import { useRef, useState } from 'react'
 import { Movie } from '../typings'
+
+/* Relevant for Exercise 1 */
 import Thumbnail from './Thumbnail'
 
 type Props =  {
@@ -39,9 +41,7 @@ function Row({ title, movies }: Props) {
         />
         <div className="flex items-center w-full p-2 gap-1.5 md:gap-2.5 overflow-x-scroll scrollbar-hide" ref={rowRef}>
           {/*TODO Exercise 1: Loop over movies prop here*/}     
-          {movies.map(movie => (
-            <Thumbnail key={movie.id} movie={movie} />
-          ))}
+       
         </div>
         <ChevronRightIcon
           className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
