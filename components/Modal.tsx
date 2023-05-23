52c67f70b13f60901ec9501f154fcdbee4b4520c
Marvin <mk306@hdm-stuttgart.de>
2023-05-23 18:51:45 +0200

    final Modal version

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react"
 import { PlusIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon, XIcon, } from "@heroicons/react/outline"
 import { FaPlay } from "react-icons/fa"
-import { useRecoilState } from "recoil"
+import { useRecoilState, useSetRecoilState } from "recoil"
 import { modalState } from "atoms/modalStateAtom"
 import { currentMovieState } from "atoms/movieStateAtom"
-import { Genre, Element, Movie } from "typings"
-import dynamic from "next/dynamic"
+import { Genre, Element } from "typings"
 import ReactPlayer from "react-player/lazy"
 
 
@@ -15,7 +14,7 @@ type Props = {
 
 export default function Modal({open}: Props){
     const [trailer, setTrailer] = useState("")
-    const [showModal, setShowModal] = useRecoilState(modalState)
+    const setShowModal = useSetRecoilState(modalState)
     const [movie, setMovie] = useRecoilState(currentMovieState)
     const [muted, setMuted] = useState(true)
     const [genres, setGenres] = useState<Genre[]>([])
@@ -48,7 +47,7 @@ export default function Modal({open}: Props){
 
     return (
         <div className={`w-full h-full fixed transition-colors flex justify-center overflow-y-scroll scrollbar-hide z-30 ${open ? "visible bg-black/70" : "invisible"}`}>
-            <div className={`absolute w-full md:w-[750px] 2xl:[850px] top-[3em] overflow-hidden rounded-lg bg-[#181818] shadow transition-all ${open ? "scale-100 opacity-100" : "scale-50 opacity-0"}`}>
+            <div className={`absolute w-full md:w-[750px] xl:w-[850px] top-[3em] overflow-hidden rounded-lg bg-[#181818] shadow transition-all ${open ? "scale-100 opacity-100" : "scale-50 opacity-0"}`}>
                 <button className="modalButton absolute z-30 right-5 top-5 h-9 w-9 border-none bg-[#181818] hover:bg-backgroundGray" onClick={() => handleClose()}>
                     <XIcon className="h-6 w-6" />
                 </button>
@@ -64,8 +63,8 @@ export default function Modal({open}: Props){
                         muted={muted}
                     />}
                    <div className="absolute top-0 w-full h-full bg-test-gradient opacity-100"></div>
-                    <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
-                        <div className="flex space-x-3">
+                    <div className="absolute bottom-10 flex w-full items-center justify-between px-14">
+                        <div className="flex gap-3">
                             <button className="flex items-center gap-x-2 rounded bg-white px-12 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
                                 <FaPlay className="h-7 w-7 text-black" />
                                 Play
@@ -86,39 +85,33 @@ export default function Modal({open}: Props){
                         </button>
                     </div>
                 </div> 
-            
-                <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10">
-                    <div className="space-y-8 text-lg">
-                        <div className="flex items-center space-x-2 text-base">
-                            <p className="font-bold text-green-400">
-                               {"95% Match"}
-                            </p>
-                            <p className="font-semibold">
-                                {movie?.release_date || movie?.first_air_date}
-                            </p>
-                            <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
-                                HD
+
+                {/* Exercise 4: Add the missing TailwindCSS using Tailwind's flex classes (flex, flex-col, flex-row, flex-wrap).*/}           
+                <div className="flex flex-col px-6 gap-6 mb-10 bg-[#181818] md:px-14 ">                                   
+                    <div className="flex flex-wrap items-center gap-2.5 text-base">
+                        <span className="font-semibold text-green-400">97% Match</span>
+                        <span className="font-semibold">{movie?.release_date || movie?.first_air_date}</span>
+                        <span className="border border-white/40 rounded-[3px] px-2 text-xs">HD</span>
+                    </div>
+                    <div className="flex flex-col justify-between gap-10 md:flex-row font-medium">
+                        <p className="basis-3/4 ">{movie?.overview}</p>
+                        <div className="basis-1/4 flex flex-col gap-3 text-base">
+                            <div>
+                                <span className="text-[gray]">Genres:</span>{' '}
+                                {genres.map((genre) => genre.name).join(', ')}
+                            </div>
+
+                            <div>
+                                <span className="text-[gray]">Original language:</span>{' '}
+                                {movie?.original_language}
+                            </div>
+
+                            <div>
+                                <span className="text-[gray]">Total votes:</span>{' '}
+                                {movie?.vote_count}
                             </div>
                         </div>
-                        <div className="flex flex-col justify-between gap-y-4 font-normal md:flex-row">
-                            <p className="w-5/6">{movie?.overview}</p>
-                            <div className="flex flex-col space-y-3 text-base font-medium">
-                                <div>
-                                    <span className="text-[gray]">Genres:</span>{' '}
-                                    {genres.map((genre) => genre.name).join(', ')}
-                                </div>
-
-                                <div>
-                                    <span className="text-[gray]">Original language:</span>{' '}
-                                    {movie?.original_language}
-                                </div>
-
-                                <div>
-                                    <span className="text-[gray]">Total votes:</span>{' '}
-                                    {movie?.vote_count}
-                                </div>
-                            </div>
-                        </div>
+                       
                     </div>
                 </div>
             </div>        
