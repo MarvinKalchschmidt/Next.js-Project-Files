f301c159b8b2b7893c8de123ad193be2550fcaaa
Marvin <mk306@hdm-stuttgart.de>
2023-05-24 02:05:11 +0200

    solution exercise 5

--- pages/watch/movie/[id].tsx
+++ pages/watch/movie/[id].tsx
@@ -12,8 +12,27 @@ type MovieData = {
     [key: string]: any;  
 }
 
-export default function Movie({movieData}: MovieData) { 
+
+export default function Movie() {    
   const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
+
+  const router = useRouter()
+  const movieID = router.query.id
+  const fetchRequest = `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`
+  const [urlTag, setUrlTag] = useState("")
+  
+  {/*Exercise 6: Move the logic inside useEffect to the getServerSideProps() function and return the fetched data as props. 
+    Avoid using State-Hooks*/}
+  useEffect(() => {
+    async function fetchMovie() {
+      const data: MovieData = await fetch(fetchRequest).then((response) => response.json())
+        if(data.videos){
+          setUrlTag(getUrlTag(data))  
+      }       
+    }
+  
+    fetchMovie()
+  })
 
   const getUrlTag = (data: MovieData) => { 
     const index = data.videos.results.findIndex((element: Element) => element.type === 'Trailer')
@@ -21,8 +40,8 @@ export default function Movie({movieData}: MovieData) {
   }
  
   return (
-      <div className={`h-screen`}>  
-      
+      <div className={`h-screen`}>   
+
         <div className="absolute px-4 pt-[18px] lg:px-16 top-0 left-0">
           <Link href="/">
               <img
@@ -37,27 +56,26 @@ export default function Movie({movieData}: MovieData) {
 
         <div className="flex flex-col items-center justify-center h-full pt-16">   
           <div className="w-[80%] aspect-video">              
-            <ReactPlayer
-                url={`https://www.youtube.com/watch?v=${getUrlTag(movieData)}`}
+            {urlTag && <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${urlTag}`}
                 width={"100%"}
                 height={"100%"}
                 style={{ objectFit: 'cover'}}
                 playing={true}
                 controls
                 muted={true}
-            />
+            />}
           </div>
         </div>          
-      </div>  
-    )
+      </div>      
+  )
 }
 
+/*Exercise 6: Implement Logik here, Note: you already have movieID from context*/
 export const getServerSideProps = async (context: any) => {
-  const movieID = context.query.id;
-  const fetchRequest = `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`
-  const data = await fetch(fetchRequest).then((response) => response.json())
+  const movieID = context.query.id; 
     
   return {
-    props: { movieData: data }    
-  }    
+    props: { }
+  }  
 }
